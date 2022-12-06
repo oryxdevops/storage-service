129d3536e026e79d0fb990717e6b8b7b18a33f27
Iyathurai Iyngaran <iyngaran55@yahoo.com>
2022-12-07 01:55:46 +0300

    Initial Commit

---
+++ src/controllers/file-uploads.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+@Controller('file-uploads')
+export class FileUploadsController {
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './files',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  handleFileUpload(@UploadedFile() file: Express.Multer.File) {
+    console.log('File : ', file);
+    return 'file upload';
+  }
+}
