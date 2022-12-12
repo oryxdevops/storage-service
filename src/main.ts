26c79933afa6597433a68e3d310594b52f2d7770
Iyathurai Iyngaran <iyngaran@oryx.edu.qa>
2022-12-12 20:43:48 +0300

    Completed file uploads with storage

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+  /*
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
+        ? 'http://localhost:3001'
+        : 'https://adap.oryxuni.com',
+    credentials: true,
+  });
+  */
   await app.listen(3000);
 }
 bootstrap();
