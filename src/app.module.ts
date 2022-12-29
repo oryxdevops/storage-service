5362f8cd9e51ab56ebef8488d1e62be8f7f0a720
Iyathurai Iyngaran <iyngaran@oryx.edu.qa>
2022-12-29 16:23:55 +0300

    added CV uploader

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,11 @@ import { PassportController } from './controllers/passport.controller';
 import { PhotoController } from './controllers/photo.controller';
 import { AcademicQualificationController } from './controllers/academic-qualification.controller';
 import { EnglishSkillController } from './controllers/english-skill.controller';
+import { CvController } from './controllers/cv.controller';
 
 @Module({
   imports: [MulterModule.register({ dest: './uploads' })],
-  controllers: [AppController, FileUploadsController, FileController, QidController, PassportController, PhotoController, AcademicQualificationController, EnglishSkillController],
+  controllers: [AppController, FileUploadsController, FileController, QidController, PassportController, PhotoController, AcademicQualificationController, EnglishSkillController, CvController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/controllers/cv.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+import { of } from 'rxjs';
+
+@Controller('cvs')
+export class CvController {
+  @Get('/:filename')
+  getFile(@Param('filename') filename, @Res() res) {
+    return of(res.sendFile(join(process.cwd(), 'files/cvs/' + filename)));
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './files/cvs',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  handleFileUpload(@UploadedFile() file: Express.Multer.File) {
+    return { file: file };
+  }
+}
